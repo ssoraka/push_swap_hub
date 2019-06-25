fe08777e4d7df738597cff1692bc6d9d8d97d89c
Suzette Soraka <ssoraka@at-f1.21-school.ru>
2019-06-25 20:33:27 +0300

    25.06.2019 20:30

--- checker.c
+++ checker.c
@@ -20,18 +20,6 @@
 #define TEST1
 
 
-void	ft_print_arr(int argc, int *argv)
-{
-	int		i;
-
-	i = 0;
-	while (i < argc)
-	{
-		printf("%d\n", argv[i]);
-		i++;
-	}
-}
-
 
 void	ft_swap(int *a, int *b)
 {
@@ -43,227 +31,6 @@ void	ft_swap(int *a, int *b)
 	*a = *b;
 	*b = temp;
 }
-
-
-int ft_next_argc(int i, int argc)
-{
-	i++;
-	if (i >= argc)
-		i = 0;
-	return (i);
-}
-
-
-int ft_prev_argc(int i, int argc)
-{
-	i--;
-	if (i < 0)
-		i = argc - 1;
-	return (i);
-}
-
-/*
-int ft_is_sorted(int *argv, int i, int argc)
-{
-	int		n;
-
-	n = 0;
-	while (n < argc - 1)
-	{
-		if (argv[i] > argv[ft_next_argc(i, argc)])
-			return (0);
-		i = ft_next_argc(i, argc);
-		n++;
-	}
-	return (1);
-}
-*/
-
-void ft_next_argv(int *argv, int argc)
-{
-	int i;
-	int temp;
-
-	temp = argv[0];
-	i = 0;
-	while (i < argc - 1)
-	{
-		argv[i] = argv[i + 1];
-		i++;
-	}
-	argv[i] = temp;
-}
-
-void ft_prev_argv(int *argv, int argc)
-{
-	int i;
-	int temp;
-
-	temp = argv[argc - 1];
-	i = argc - 1;
-	while (i > 0)
-	{
-		argv[i] = argv[i - 1];
-		i--;
-	}
-	argv[0] = temp;
-}
-
-
-/*
-void	ft_sort_argv(int argc, int *argv)
-{
-	int		i;
-	int		action;
-	int		*active;
-	int		*next;
-	int		*prev;
-
-	int		j = 0;
-
-	action = 0;
-	i = 0;
-	while (j < 1000)
-	{
-		active = &(argv[i]);
-		next = &(argv[ft_next_argc(i, argc)]);
-		prev = &(argv[ft_prev_argc(i, argc)]);
-
-		if (*active > *next && i != argc - 1)
-		{
-			ft_swap(active, next);
-			action++;
-		}
-
-		if (ft_is_sorted(argv, i, argc))
-			break ;
-		if (*active < *prev && i != 0)
-			i = ft_prev_argc(i, argc);
-		else
-			i = ft_next_argc(i, argc);
-
-		action++;
-		j++;
-	}
-	printf("\n%d\n", action);
-}
-*/
-
-
-
-/*
-int check(int *tab, int min, int len)
-{
-    int i;
-
-    i = 0;
-    while (tab[i] != min)
-        i++;
-    i++;
-    if (i >= len)
-        i = i - len;
-    if (i == 0)
-        i++;
-    while (tab[i - 1] < tab[i])
-    {
-
-        i++;
-        if (i >= len)
-            i = i - len;
-    }
-    if (tab[i] == min)
-        return (len - i);
-    return (0);
-}
-*/
-
-
-
-/*
-int        main222()
-{
-    //int tab[10] = {5, 56, 3, 45, 30, -4, 18, -100, 0, 13};
-    //int tab[4] = {7, 3, 5 ,1};
-    int tab[6] = {8, 5, 3, 4, 2, 1};
-    //int tab[] = {10,3,4,5,9,13,0,6,78,8};
-    //int tab[] = {3,2,0};
-    int i;
-    int len = 6;
-    int t;
-    int j;
-    int count;
-
-    i = 1;
-    count = 0;
-    while (!check(tab, 1, len))
-    {
-        t = tab[i];
-        count++;
-        j = i - 1;
-        while (j >= 0 && tab[j] > t)
-        {
-            tab[j + 1] = tab[j];
-            j--;
-            count++;
-        }
-        tab[j + 1] = t;
-        i++;
-        if (i >= len)
-            i = i - len;
-    }
-    printf(“%d\n”, count + check(tab, 1, len));
-    i = 0;
-    while (i < len)
-    {
-        printf(“%d\t”, tab[i]);
-        i++;
-    }
-}*/
-
-/*
-int		main(int argc, char **argv)
-{
-	int arr[] = {10,3,4,5,9,13,0,6,78,8};
-	t_arr *arra = 0;
-	int *arrb = 0;
-
-	arra = arr;
-	int count = 10;
-	int min = 0;
-
-
-	ft_sa();
-	ft_sb();
-	ft_ss();
-
-
-
-	ft_print_arr(count, arr);
-	//ft_sort_argv(count, arr);
-	printf("\n");
-	ft_prev_argv(arr, count);
-	ft_print_arr(count, arr);
-
-	printf("\n");
-	ft_prev_argv(arr, count);
-	ft_print_arr(count, arr);
-
-	printf("\n");
-	ft_prev_argv(arr, count);
-	ft_print_arr(count, arr);
-
-	return (0);
-}
-*/
-
-typedef struct		s_arr
-{
-	int				*arr;
-	int				count;
-	int				first_elem;
-	int				min_elem;
-	int				max_elem;
-}					t_arr;
 
 
 typedef struct		s_elem
@@ -354,6 +121,53 @@ void ft_print_list(t_elem *begin, char stek)
 	printf("\n");
 }
 
+void ft_print_list2(t_elem *begin, char stek)
+{
+	int first_value;
+	int i;
+
+	i = 0;
+	printf(" %c:  ", stek);
+	if (begin == NULL)
+	{
+		printf("NULL\n");
+		return ;
+	}
+	first_value = begin->value;
+	while (i == 0 || (begin && first_value != begin->value))
+	{
+		printf("%d\t", begin->value);
+		begin = begin->next;
+		i++;
+	}
+	printf("\n");
+}
+
+/*
+void ft_print_list3(t_elem *arra, t_elem *arrb, int count)
+{
+	int first_value_a;
+	int first_value_b;
+	int i;
+
+	i = 0;
+	printf("_%c_\n", stek);
+	if (begin == NULL)
+	{
+		printf("NULL\n");
+		return ;
+	}
+	first_value = begin->value;
+	while (i == 0 || (begin && first_value != begin->value))
+	{
+		printf("%d\n", begin->value);
+		begin = begin->next;
+		i++;
+	}
+	printf("\n");
+}
+*/
+
 int ft_is_sorted(t_elem *begin)
 {
 	int first_value;
@@ -375,24 +189,6 @@ int ft_is_sorted(t_elem *begin)
 	return (1);
 }
 
-/*
-int ft_need_round_sorted(t_elem *begin)
-{
-	int first_value;
-	int i;
-
-	i = 0;
-	first_value = begin->value;
-	while (i == 0 || (begin && first_value != begin->value))
-	{
-		if (ft_is_sorted(begin))
-			return (i);
-		begin = begin->next;
-		i++;
-	}
-	return (-1);
-}
-*/
 
 int ft_need_round_for_sort(t_elem *begin)
 {
@@ -525,72 +321,6 @@ void ft_push_list(t_elem **arra, t_elem **arrb, char stek)
 }
 
 
-t_elem *ft_bubble_sort(t_elem *arr)
-{
-	int		j = 0;
-
-	while (1)
-	{
-		if (arr->value > arr->next->value && arr->next->value != arr->min_value)
-			ft_swap_list(arr, 'a');
-
-		if (ft_is_sorted(arr))
-			break ;
-		if (arr->value < arr->prev->value && arr->value != arr->min_value)
-			ft_revers_round_list(&arr, 'a');
-		else
-		{
-			if (ft_need_round_for_sort(arr) > 0)
-				ft_round_list(&arr, 'a');
-			else if (ft_need_round_for_sort(arr) < 0)
-				ft_revers_round_list(&arr, 'a');
-			else
-				ft_round_list(&arr, 'a');
-		}
-
-		//j++;
-	}
-	return (arr);
-	//printf("\n%d\n", action);
-}
-
-
-t_elem *ft_vstavka_sort(t_elem *arra, t_elem *arrb)
-{
-	while (arra && arra->next && arra->next->next != arra)
-	{
-		if (arra->value == arra->min_value)
-			ft_round_list(&arra, 'a');
-		ft_push_list(&arra, &arrb, 'b');
-	}
-	//ft_print_list(arra, 'a');
-	//ft_print_list(arrb, 'b');
-	while (arrb)
-	{
-		if (arrb->value > arra->value && arra->value != arra->min_value)
-			ft_round_list(&arra, 'a');
-		else if (arrb->value < arra->value && arrb->value < arra->prev->value)
-			ft_revers_round_list(&arra, 'a');
-		else if (arrb->value > arra->value && arra->value == arra->min_value)
-			ft_revers_round_list(&arra, 'a');
-		if (arrb->value < arra->value && arrb->value > arra->prev->value)
-			ft_push_list(&arrb, &arra, 'a');
-		else if (arrb->value > arra->prev->value && arra->value == arra->min_value)
-			ft_push_list(&arrb, &arra, 'a');
-	}
-
-	while (arra->value != arra->min_value)
-	{
-		if (ft_need_round_for_sort(arra) > 0)
-			ft_round_list(&arra, 'a');
-		else if (ft_need_round_for_sort(arra) < 0)
-			ft_revers_round_list(&arra, 'a');
-	}
-
-	return (arra);
-}
-
-
 
 char **open_and_read_command(char *name)
 {
@@ -604,18 +334,9 @@ char **open_and_read_command(char *name)
 	while ((red = read(file, buf, 1000)))
 	{
 		buf[red] = '\0';
-		//printf("%s\n", ar[i]);
 		str = ft_strjoin_free(str, buf, 1, 0);
 	}
 	return (ft_strsplit(str, '\n'));
-}
-
-
-
-void ft_error(void)
-{
-	ft_putstr_fd("Error\n", 2);
-	exit (0);
 }
 
 
@@ -625,7 +346,7 @@ int ft_is_not_number(char **str)
 	char *tmp;
 
 	i = 0;
-	while (**str == '0' && (*str + 1) && ft_isdigit(*(*str + 1)))
+	while (**str == '0' && (*str + 1) && ft_isdigit(*str + 1))
 		(*str)++;
 	tmp = *str;
 	if (!ft_isdigit(*tmp))
@@ -676,6 +397,13 @@ int ft_str_not_int_number(char *str)
 }
 
 
+void ft_error(void)
+{
+	ft_putstr_fd("Error\n", 2);
+	exit (0);
+}
+
+
 int *ft_arr_int_from_str(char **argv, int *argc)
 {
 	int *arr;
@@ -684,6 +412,7 @@ int *ft_arr_int_from_str(char **argv, int *argc)
 	i = 0;
 	while (argv[i])
 	{
+		printf("%s\n",argv[i] );
 		if (ft_str_not_int_number(argv[i]))
 			ft_error();
 		i++;
@@ -699,7 +428,6 @@ int *ft_arr_int_from_str(char **argv, int *argc)
 	}
 	return (arr);
 }
-
 
 int ft_have_repeat_num(int *arr, int argc)
 {
@@ -722,6 +450,22 @@ int ft_have_repeat_num(int *arr, int argc)
 }
 
 
+int ft_not_valid_command(char **commands)
+{
+	while (*commands)
+	{
+		if (ft_strcmp(*commands, "ra") && ft_strcmp(*commands, "rb") &&
+		ft_strcmp(*commands, "sa") && ft_strcmp(*commands, "sb") &&
+		ft_strcmp(*commands, "ss") && ft_strcmp(*commands, "pa") &&
+		ft_strcmp(*commands, "pb") && ft_strcmp(*commands, "rra") &&
+		ft_strcmp(*commands, "rrb") && ft_strcmp(*commands, "rrr") &&
+		ft_strcmp(*commands, "rr"))
+			return (1);
+		(commands)++;
+	}
+	return (0);
+}
+
 
 int		main(int argc, char **argv)
 {
@@ -735,7 +479,6 @@ int		main(int argc, char **argv)
 	arr = ft_arr_int_from_str(arv, &argc);
 	if (ft_have_repeat_num(arr, argc))
 		ft_error();
-
 /*
 	int arr[] = {2,1,3,6,5,8};
 	argc = 6;
@@ -749,20 +492,47 @@ int		main(int argc, char **argv)
 	arrb = NULL;
 
 
-	//char *str = ft_strdup("222\t ");
-	//printf("%s__%d__%d\n", str, ft_str_not_int_number(str), atoi(str));
 
-	//ft_print_list(arra);
+	///checker
+	char **cmd;
+	int i = 0;
 
-	/*printf("%d\n", ft_need_round_sorted(arra));
-	int rounds;
-	int needs = ft_is_list_sorted_and_rounded(arra, &rounds);
-	printf("%d, %d\n", needs, rounds);*/
+	cmd = open_and_read_command("command.txt");
+	if (ft_not_valid_command(cmd))
+		ft_error();
+	while (cmd && cmd[i])
+	{
+		printf("%d__%s\n", i, cmd[i]);
+		if (ft_strcmp(cmd[i], "sa") == 0)
+			ft_swap_list(arra, 0);
+		if (ft_strcmp(cmd[i], "sb") == 0)
+			ft_swap_list(arrb, 0);
+		if (ft_strcmp(cmd[i], "ra") == 0)
+			ft_round_list(&arra, 0);
+		if (ft_strcmp(cmd[i], "rb") == 0)
+			ft_round_list(&arrb, 0);
+		if (ft_strcmp(cmd[i], "rrb") == 0)
+			ft_revers_round_list(&arrb, 0);
+		if (ft_strcmp(cmd[i], "rra") == 0)
+			ft_revers_round_list(&arra, 0);
+		if (ft_strcmp(cmd[i], "pa") == 0)
+			ft_push_list(&arrb, &arra, 0);
+		if (ft_strcmp(cmd[i], "pb") == 0)
+			ft_push_list(&arra, &arrb, 0);
+		ft_print_list2(arra, 'a');
+		ft_print_list2(arrb, 'b');
+		i++;
+	}
 
-	//arra = ft_bubble_sort(arra);
-	arra = ft_vstavka_sort(arra, arrb);
 
-//	ft_print_list(arra, 'a');
-//	ft_print_list(arrb, 'b');
+	ft_print_list2(arra, 'a');
+	ft_print_list2(arrb, 'b');
+
+	printf("строк - %d\n", i);
+	if (ft_is_sorted(arra) && !arrb)
+		printf("ОК\n");
+	else
+		printf("КО\n");
+
 	return (0);
 }
